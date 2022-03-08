94da70a1d9d29ea2790899faecac6b38e96e6604
Hossameldin Shahin <hosamlshahin@gmail.com>
2022-03-08 14:19:07 -0500

    init

--- .projenrc.js
+++ .projenrc.js
@@ -1,11 +1,14 @@
-const { awscdk } = require('projen');
+const { awscdk, ProjectType } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Hossameldin Shahin',
   authorAddress: 'hosamlshahin@gmail.com',
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   name: 'cdk-projen',
-  repositoryUrl: 'https://github.com/hosamlshahin/cdk-projen.git',
+  repositoryUrl: 'https://github.com/hosamshahin/cdk-projen.git',
+  githubOptions: { mergify: true },
+  eslint: true,
+  gitignore: ['.idea'],
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
